a2dccbce6989b8fa8b90f5ec52cf619cc5db188d
i0nutt <ionuttiplea2001@gmail.com>
2021-01-03 13:48:50 +0200

    mesaj

--- application/app/src/main/java/policlinica/users/User.java
+++ application/app/src/main/java/policlinica/users/User.java
@@ -13,12 +13,23 @@ public class User {
 	private final static String USER = "root";
 	private final static String PASSWORD = "1234";
 	
-	
-	
+	private Connection setup() throws SQLException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception ex) {
+			System.err.println("An Exception occured during JDBC Driver loading." + " Details are provided below:");
+			ex.printStackTrace(System.err);
+		}
+		return DriverManager.getConnection(URL + DB_NAME + USER + PASSWORD);
+	}
+	private ResultSet getResult(String statement, Connection c) throws SQLException{
+		PreparedStatement s;
+		s = c.prepareStatement(statement);
+		return s.executeQuery();
+	}	
     public static void main(String[] args) {
 		
 		Connection c;
-		
 		try {
 			c=DriverManager.getConnection(URL+DB_NAME, USER, PASSWORD);
 
