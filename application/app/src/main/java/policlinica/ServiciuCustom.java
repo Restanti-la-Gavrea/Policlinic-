f9b2092b5af326fe8709441cfb48a7cce8254fa5
i0nutt <ionuttiplea2001@gmail.com>
2021-01-15 15:32:29 +0200

    hfjksdf

--- application/app/src/main/java/policlinica/ServiciuCustom.java
+++ application/app/src/main/java/policlinica/ServiciuCustom.java
@@ -1,11 +1,36 @@
 package policlinica;
-
+import policlinica.users.*;
+import java.sql.*;
 public class ServiciuCustom extends Serviciu{
 	private String pret; 
 	private String durata; 
 	private String id = "-1"; 
-	ServiciuCustom(String nrContract){
-		 
+	ServiciuCustom(String nrContract,String nrServiciu){
+		User u = new User();
+		 ResultSet rs = u.executeSelect("Select * from Serviciu inner join Serviciu Custom on Serviciu.nrServiciu = ServiciuCustom.nrServiciu"
+		 		+ "where nrContract = " + nrContract + " Serviciu.nrServiciu = " + nrServiciu+ ";");
+		 try {
+			if(rs.next()) {
+				 this.setNrServiciu(nrServiciu);
+				 this.setNume(rs.getString("nume"));
+				 this.pret = rs.getString("newpret");
+				 this.durata = rs.getString("newdurata"); 
+				 this.id = rs.getString("id");
+			 }
+			else {
+				rs = u.executeSelect("Select * from Serviciu where nrServiciu = " + nrServiciu + ";");
+				if(rs.next()) {
+					 this.setNrServiciu(nrServiciu);
+					 this.setNume(rs.getString("nume"));
+					 this.pret = rs.getString("pret");
+					 this.durata = rs.getString("durata"); 
+					 this.id = rs.getString("-1");
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public String getPret() {
 		return pret;
